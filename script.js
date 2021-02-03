d6dc87f71352b8125753ca7997dbc19b1ce9f79e
yoelng <yoelgoitom25@gmail.com>
2021-02-02 22:49:47 -0800

    JS selectors

---
+++ script.js
@@ -0,0 +1,21 @@
+var skills = document.getElementById("skillList");
+console.log(skills);
+
+var allDivs = document.getElementsByClassName("resume");
+console.log(allDivs);
+
+var allLi = document.getElementsByTagName("li");
+console.log(allLi);
+
+var selectID = document.querySelector("#obj");
+var selectClss = document.querySelector(".resume");
+var selectTag = document.querySelector("div");
+
+console.log(selectID);
+console.log(selectClss);
+console.log(selectTag);
+
+var allElelment = document.querySelectorAll(".resume");
+var selectLi = document.querySelectorAll("li");
+console.log(allElelment);
+console.log(allLi);
